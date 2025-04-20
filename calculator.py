129ce38a371636924607a0189bd2f1709d5416f7
kangaick <carokangach@gmail.com>
2025-04-20 11:59:02 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,39 +1,39 @@
-operator = input("Enter an operator:")
+Operator= input("Enter a operator:")
 num1 = float(input("Enter first number:"))
 num2 = float(input("Enter second number:"))
 
 if operator == '+':
     result = num1+num2
-    print(round(result,3))
+    print(result)
     
 elif operator == '-':
     result = num1-num2
-    print(round(result,3))
+    print(round(result))
 
 
 elif operator == "*":
     result = num1*num2
-    print(round(result,3))
+    print(round(result)
 
 
 elif operator == "/":
     result = num1/num2
-    print(round(result,3))
+    print(round(result)
 
 
 elif operator == "%":
     result = num1%num2
-    print(round(result,3))
+    print(round(result)
 
 
 elif operator == "//":
     result = num1//num2
-    print(round(result,3))
+    print(round(result)
 
 
 elif operator == "**":
     result = num1**num2
-    print(round(result,3))
+    print(round(result)
 
 
 else:
